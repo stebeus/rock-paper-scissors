dae118f603388d6001b267fde24f1b7f82654acc
stebeus <stebeus@mailfence.com>
2025-10-09 16:36:37 -0300

    Code the constants algorithm

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 // Store in three constants each one of the following strings: Rock, Paper and Scissors
+const rock = 'Rock';
+const paper = 'Paper';
+const scissors = 'Scissors';
+
 // Store a three random number generator in the get random choice constant
+const getRandomChoice = Math.floor(Math.random() * 3);
