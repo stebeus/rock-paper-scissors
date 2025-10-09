c01486030c21037d373b428df54d307992ff82bf
stebeus <stebeus@mailfence.com>
2025-10-09 16:38:21 -0300

    Write the getComputerChoice algorithm

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ const scissors = 'Scissors';
 
 // Store a three random number generator in the get random choice constant
 const getRandomChoice = Math.floor(Math.random() * 3);
+
+// CALL the get computer choice to return the three global constants randomly
+// IF random choice === 0 THEN return rock
+// ELSE IF random choice === 1 return paper
+// ELSE return scissors
