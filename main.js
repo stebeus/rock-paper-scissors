bb08ca9f1f12bfb2b422c77bce0e384c9224c8cb
stebeus <stebeus@mailfence.com>
2025-10-09 16:43:16 -0300

    Move getRandomChoice to getComputerChoice function
    When getRandomChoice scope is global, the function always return the same value.
    Move the constant to the function to return different values when the function is recalled.

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const rock = 'Rock';
 const paper = 'Paper';
 const scissors = 'Scissors';
 
-// Store a three random number generator in the get random choice constant
-const getRandomChoice = Math.floor(Math.random() * 3);
-
 // CALL the get computer choice to return the three global constants randomly
 function getComputerChoice() {
+  // Store a three random number generator in the get random choice constant
+  const getRandomChoice = Math.floor(Math.random() * 3);
+
   if (getRandomChoice === 0) {
     return rock;
   } else if (getRandomChoice === 1) {
