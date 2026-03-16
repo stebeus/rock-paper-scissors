87d2e212e93afae1a95be05b64015609693629e4
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-16 16:33:35 -0300

    feat: add weapon contols

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { renderAnnouncement } from './components/announcement.js';
+import { createWeaponControls } from './components/controls.js';
 import { createPlayerScores } from './components/scoreboard.js';
-import { playerScores } from './core/game.js';
+import { playerScores, weapons } from './core/game.js';
 
 renderAnnouncement();
 createPlayerScores(playerScores);
+createWeaponControls(weapons);
