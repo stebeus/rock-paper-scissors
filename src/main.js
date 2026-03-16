e10148f7f4c3020933b38c156a26ecf4b7ff9f35
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-16 16:33:23 -0300

    feat: add player scores

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 import { renderAnnouncement } from './components/announcement.js';
+import { createPlayerScores } from './components/scoreboard.js';
+import { playerScores } from './core/game.js';
 
 renderAnnouncement();
+createPlayerScores(playerScores);
