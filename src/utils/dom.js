a4b6eecbaa13c925bf4204ec1ef337a2f34d9a08
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-16 10:26:54 -0300

    feat: add raw HTML getter

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,2 +1,5 @@
 const serializeHtml = (value) =>
   value instanceof Element ? value.outerHTML : value;
+
+const getRawHtml = (strings, ...values) =>
+  String.raw({ raw: strings }, ...values.map(serializeHtml));
