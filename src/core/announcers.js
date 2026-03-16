fc587102ae5e626a943716e6c08218a029180c92
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-16 10:15:23 -0300

    feat: add round winner announcer

--- src/core/announcers.js
+++ src/core/announcers.js
@@ -4,3 +4,19 @@ class Announcement {
     this.description = description;
   }
 }
+
+function announceRoundWinner(humanChoice, computerChoice, result) {
+  let content = ['Tie!', `${humanChoice} ties with ${computerChoice}`];
+
+  if (result === 1) {
+    content = ['You win!', `${humanChoice} beats ${computerChoice}`];
+  }
+
+  if (result === 2) {
+    content = ['You lose!', `${computerChoice} beats ${humanChoice}`];
+  }
+
+  return new Announcement(...content);
+}
+
+export { announceRoundWinner };
