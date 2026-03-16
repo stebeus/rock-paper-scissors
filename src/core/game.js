c32980a6a2aabca2bb87c87139e0ae391f4862db
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-16 10:12:57 -0300

    feat: add choice evaluator

--- src/core/game.js
+++ src/core/game.js
@@ -7,4 +7,15 @@ const getComputerChoice = (choices) => choices[getRandomIndex(choices)];
 
 const isGameOver = (scores) => getScoreDifference(scores) === 2;
 
+function evaluateChoices([human, computer], { length }, scores) {
+  const result = (human + computer + length) % length;
+
+  for (const playerIndex in scores) {
+    const outcome = Number(playerIndex) + 1;
+    if (outcome === result) scores[playerIndex]++;
+  }
+
+  return result;
+}
+
 export { getComputerChoice, isGameOver, playerScores, weapons };
