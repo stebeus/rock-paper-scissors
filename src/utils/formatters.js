51afa0dca8ce43ce12328d29a0e2cf412c3a0259
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-16 10:38:36 -0300

    feat: add score formatter

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,3 +1,10 @@
+function formatScore(playerIndex, playerScores) {
+  const parsedIndex = Number(playerIndex);
+  const playerName = parsedIndex === 0 ? 'Your' : 'Computer';
+
+  return `${playerName} score: ${playerScores[playerIndex]}`;
+}
+
 const formatButtonLabel = (label) => label.split('-').join(' ');
 
-export { formatButtonLabel };
+export { formatButtonLabel, formatScore };
