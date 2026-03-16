c948d017d034defa67d8c73ea77d50ca4dffba70
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-16 16:33:57 -0300

    feat: delegate event listeners

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { renderAnnouncement } from './components/announcement.js';
 import { createWeaponControls } from './components/controls.js';
 import { createPlayerScores } from './components/scoreboard.js';
+import { handleRestart, handleWeaponChoice } from './controllers/handlers.js';
 import { playerScores, weapons } from './core/game.js';
 
 renderAnnouncement();
 createPlayerScores(playerScores);
 createWeaponControls(weapons);
+
+// Event delegation
+document.addEventListener('click', (event) => {
+  handleWeaponChoice(event);
+  handleRestart(event);
+});
