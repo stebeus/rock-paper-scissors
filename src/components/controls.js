26938ad60bdddc197fe1f8b0cf221b471bc4ad6c
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-16 10:34:15 -0300

    feat: add weapon controls creator

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,1 +1,12 @@
+import { createButton } from './button.js';
+
 const controls = document.querySelector('.controls');
+
+function createWeaponControls(weapons) {
+  for (const weapon of weapons) {
+    const weaponControl = createButton('weapon', weapon);
+    controls.append(weaponControl);
+  }
+}
+
+export { createWeaponControls };
