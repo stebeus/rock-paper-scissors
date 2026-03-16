0a909d9200cb1c79a367df1a958a1b44c5a6e5e5
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-16 15:28:17 -0300

    refactor: set initial announcement in its own render

--- src/components/announcement.js
+++ src/components/announcement.js
@@ -1,11 +1,12 @@
-function renderAnnouncement({ title, description }) {
+function renderAnnouncement({ title, description } = {}) {
   const announcementTitle = document.querySelector('.announcement__title');
   const announcementDescription = document.querySelector(
     '.announcement__description',
   );
 
-  announcementTitle.textContent = title;
-  announcementDescription.textContent = description;
+  announcementTitle.textContent = title ?? 'Choose a weapon';
+  announcementDescription.textContent =
+    description ?? 'First to score a 2-point lead wins';
 }
 
 export { renderAnnouncement };
