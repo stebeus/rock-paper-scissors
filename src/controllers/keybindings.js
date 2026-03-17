022970b8f8aec495d5478b93b2c057b652570f7a
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-17 09:54:32 -0300

    refactor: call related weapon controls directly in the binder

--- src/controllers/keybindings.js
+++ src/controllers/keybindings.js
@@ -1,3 +1,6 @@
+import { weapons } from '../core/game.js';
+import { controlGame } from './game-controller.js';
+
 function bindKey(eventKey, key, action, ...parameters) {
   const keybinding = eventKey.toLowerCase();
   if (keybinding !== key) return;
@@ -5,10 +8,10 @@ function bindKey(eventKey, key, action, ...parameters) {
   action(...parameters);
 }
 
-function bindWeaponControlKeys(eventKey, weapons, gameController) {
+function bindWeaponControlKeys(eventKey) {
   for (const weapon of weapons) {
     const key = weapon.charAt(0);
-    bindKey(eventKey, key, gameController, weapon);
+    bindKey(eventKey, key, controlGame, weapon);
   }
 }
 
