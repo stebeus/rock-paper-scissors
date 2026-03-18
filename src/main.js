8da3b9764c5d95c2b4c4b5302ab0dc44d6d85a62
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-18 13:16:30 -0300

    feat: create weapon controls

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { renderAnnouncement } from './components/announcement.js';
 import { renderComputerOutput } from './components/computer-output.js';
+import { createWeaponControls } from './components/controls.js';
 import { createPlayerScores } from './components/scoreboard.js';
-import { playerScores } from './core/game.js';
+import { playerScores, weapons } from './core/game.js';
 
 renderAnnouncement();
 renderComputerOutput;
 createPlayerScores(playerScores);
+createWeaponControls(weapons);
