0997bae14a512a235b070a06349cef706c2c1186
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-18 14:12:45 -0300

    refactor: change "manage" prefix to "control"

--- src/controllers/game-controller.js
+++ src/controllers/game-controller.js
@@ -1,7 +1,7 @@
 import { createControl, replaceControls } from '../components/controls.js';
 import { isGameOver } from '../core/game.js';
 
-function manageGameOver(scores) {
+function controlGameOver(scores) {
   if (!isGameOver(scores)) return;
   replaceControls(createControl, 'restart', 'Restart game');
 }
