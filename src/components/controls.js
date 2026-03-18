cae552a617a3ca6e8de85b683585217237d81df1
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-18 12:47:01 -0300

    feat: add controls replacer

--- src/components/controls.js
+++ src/components/controls.js
@@ -13,4 +13,12 @@ function createWeaponControls(weapons) {
   }
 }
 
-export { createControl, createWeaponControls };
+function replaceControls(controlCreator, ...parameters) {
+  const hasControls = controls.hasChildNodes();
+  if (!hasControls) return;
+
+  controls.innerHTML = '';
+  controlCreator(...parameters);
+}
+
+export { createControl, createWeaponControls, replaceControls };
