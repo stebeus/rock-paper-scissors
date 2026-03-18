cc67c13e127c341a5bf4632c0b0aa439bb87347b
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-18 12:44:09 -0300

    feat: add control creator

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,1 +1,10 @@
+import { createButton } from './button.js';
+
 const controls = document.querySelector('.controls');
+
+function createControl(dataAttributeName, attributeValue) {
+  const control = createButton(dataAttributeName, attributeValue);
+  controls.append(control);
+}
+
+export { createControl };
