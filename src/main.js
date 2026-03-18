9448a74bf6bd737f491ce6d912b113a904edd2d5
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-18 13:16:06 -0300

    feat: create player scores

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { renderAnnouncement } from './components/announcement.js';
 import { renderComputerOutput } from './components/computer-output.js';
+import { createPlayerScores } from './components/scoreboard.js';
+import { playerScores } from './core/game.js';
 
 renderAnnouncement();
 renderComputerOutput;
+createPlayerScores(playerScores);
