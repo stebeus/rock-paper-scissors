4dd301454da7c14f70ad2dce4bd3763990cb3dd5
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-18 14:15:04 -0300

    feat: add announcement controller

--- src/controllers/game-controller.js
+++ src/controllers/game-controller.js
@@ -1,5 +1,11 @@
+import { renderAnnouncement } from '../components/announcement.js';
 import { createControl, replaceControls } from '../components/controls.js';
 import { isGameOver } from '../core/game.js';
+
+function controlAnnouncement(announcer, ...announcerParameters) {
+  const announcement = announcer(...announcerParameters);
+  renderAnnouncement(announcement);
+}
 
 function controlGameOver(scores) {
   if (!isGameOver(scores)) return;
