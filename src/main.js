99dc9de218f79193adb63d4c3565a2c4cba04676
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-18 13:15:35 -0300

    feat: render computer output

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import { renderAnnouncement } from './components/announcement.js';
+import { renderComputerOutput } from './components/computer-output.js';
 
 renderAnnouncement();
+renderComputerOutput;
