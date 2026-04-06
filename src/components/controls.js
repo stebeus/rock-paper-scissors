3a4ad5afa7111745745ce99346da1811fbe3bf3b
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-06 12:12:17 -0300

    feat: add control creator

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,1 +1,11 @@
+import { elementFromHtml } from '../utils.js';
+
 const controls = document.querySelector('#controls');
+
+function createControl(label, handler) {
+  return elementFromHtml(
+    `<button class="control" type="button" onclick="${handler}">
+      ${label}
+    </button>`,
+  );
+}
