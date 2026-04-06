62d19263f34104580b7289f53781098dc7f59124
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-06 11:19:51 -0300

    feat: add computer choice getter

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const scores = [0, 0];
 const weapons = ['rock', 'paper', 'scissors'];
 
 const resetScores = (scores) => scores.fill(0);
+
+function getComputerChoice({ length }) {
+  const randomChoice = Math.random() * length;
+  return Math.floor(randomChoice);
+}
